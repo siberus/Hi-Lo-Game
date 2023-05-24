e81c8717186d582c115c70ad4db9860c19fcb59f
siberus <mr.siberus@yandex.ru>
2023-05-24 23:12:07 +0700

    Add gitignore

--- main.cpp
+++ main.cpp
@@ -11,7 +11,64 @@ int getRandomNumber()
     return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
 
+int getNumber()
+{
+    int f_number;
+    while (true)
+    {
+       std::cout << "Enter a number from 0 to 100." << std::endl;
+       std::cin >> f_number;
+       //Проверка на int
+       if (std::cin.fail())
+       {
+            std::cin.clear();
+            std::cin.ignore(32767,'\n');
+            std::cout << "Input is invalid. Please try again.\n";
+       }
+       else
+       {
+            std::cin.ignore(32767,'\n');
+            if ((0<=f_number) && (f_number = 100))
+            {
+                return f_number;
+            }
+            else
+            {
+                std::cout << "Number must be between 0 and 100. Please try again.\n";
+            }
+       }
+    }
+    //return f_digit;
+}
+char getRepeat()
+{
+    std::cout << "Would you like to play again (y/n)?" << std::endl;
+    char f_choise;
+    std::cin >> f_choise;
+    std::cin.ignore(32767,'\n'); // удаляем лишний балласт
+    if (f_choise == 'y' ||f_choise == 'n' )
+    {
+        return f_choise;
+    }
+    else
+    {
+        std::cout << "Oops, that input is invalid. Please try again.\n";
+    }
 
+}
+
+void comparation (int p_number, int p_answer)
+{
+    if (p_number > p_answer)
+    {
+        std::cout << "Your guess is too high." << std::endl;
+    }
+    else
+    {
+        std::cout << "Your guess is too low." << std::endl;
+    }
+    
+}
 
 int main()
 {
@@ -19,18 +76,29 @@ int main()
     std::cout << "You have 7 attempts to guess a number from 0 to 100." << std::endl;
     while (true)
     {
+
         // устанавливаем значение системных часов в качестве стартового числа
         srand(static_cast<unsigned int>(time(0)));
-        int digit =  getRandomNumber();
+        int answer =  getRandomNumber();
         for (int attempt = 0; attempt <= 7; attempt++)
         {
-            //Функция ввода числа
-
+            std::cout << "Guess #" << attempt << "." << std::endl; 
+            int number = getNumber();
+            if (answer == number)
+            {
+                std::cout << "You are win! Number is "<< answer << "!" << std::endl;
+                break;
+            }
+            else
+            {
+                comparation(number, answer);
+            }
         }
-        
-
-    }
-    
-        
+        char repeat = getRepeat();
+        if (repeat == 'n')
+        {
+            break;
+        }
+    }   
     return 0;
 }
